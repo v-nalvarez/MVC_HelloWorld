d792f64030d924c55a3866c4b36d4517a5a39fdd
Nelson Alvarez Gutierrez <v-nealva@microsoft.com>
2020-09-04 11:45:03 -0600

    First hit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_HelloWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
